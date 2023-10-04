fae936b13e5dd73829a17339b69f68ece05a8e05
KSierpowski <sierpowskikacper@gmail.com>
2023-10-04 18:51:13 +0200

    Added add task button

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -4,16 +4,14 @@ import {tasks as tasksData} from '../data/tasks';
 export function TasksList(){
     const [tasks, setTasks] = useState(tasksData);
 
-    const handleCompleteTask = index => {
+    const handleToggleTask = index => {
 
         const newTasks = [...tasks];
-        newTasks[index].completed =true;
+        newTasks[index].completed = !newTasks[index].completed;
        setTasks(newTasks); 
 
 
     };
-
-
 
 
     const handleDeleteTask = (index) => {
@@ -21,24 +19,41 @@ export function TasksList(){
       newTasks.splice(index, 1);
       setTasks(newTasks);
     };
-    return(
-        <ul>
 
-        {tasks.map((task, index)=>{
-          return(
-          <>
-      
+    const handleAddddTask = (title) => {
+        const newTasks = [...tasks];
+        newTasks.push({
+            title: title,
+            description: "",
+            completed: false
+        });
+        setTasks(newTasks);
+    };
+
+
+
+    return(
+    <>
+        <button onClick={() => handleAddddTask("Add new task")}>+</button>
+        {tasks.lenght === 0? <div>Empty list</div> : 
+        
+        <ul>
+        
+        {tasks.map(({title, completed }, index) => (
+       
             <li key={index} style={{
-                textDecoration: task.completed? 'line-through' : 'none'
-            }}>{task.title}</li>
-            <button onClick={() => handleDeleteTask(index)}>X</button>
-            <button onClick={() => handleCompleteTask(index)}>Y</button>
-          </>);
+                textDecoration: completed? 'line-through' : 'none'
+            }}>{title}
+            <button onClick={() => handleToggleTask(index)}>{completed ? 'Undo' : 'Complete'} </button>
+            <button onClick={() => handleDeleteTask(index)}>Delete </button>
+            </li>
            
-     })}
+     ))}
     
     </ul>
-    )
+        }
+    </>
+    );
 
 
 
